2c454239b1e567fa4afc88c1f0cbc81b41885db4
Cut the Wire <102637600+CaDiVae@users.noreply.github.com>
2024-03-31 11:58:27 +0900

    basecode 수정

--- OCR/base_code.py
+++ OCR/base_code.py
@@ -8,18 +8,14 @@ from PIL import Image
 screenshot = pyautogui.screenshot()
 screenshot = np.array(screenshot)
 screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
-
-# 스크린샷 저장
-cv2.imwrite('OCR/desktop_screenshot.png', screenshot)
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 # 이미지 로드 및 OCR을 통한 텍스트 인식
-img = Image.open('OCR/desktop_screenshot.png')
-text = pytesseract.image_to_string(img)
+img = Image.open('OCR\대한민국헌법(제00010호).png')
+text = pytesseract.image_to_string(img, lang="eng+kor")
 
 # 지정한 텍스트가 있는지 확인 (영어)
-search_text = 'OCR'
-print(text)
+search_text = '민주'
 if search_text in text:
     print(f"'{search_text}'를 찾았습니다!")
 else:
